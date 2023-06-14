490b08230a06f8e248f40d2b6c9c0891ebe79633
ldh7228 <ldh7228@gmail.com>
2023-06-14 17:07:46 +0900

    Update axios dist

--- src/axios/dist/index.ts
+++ src/axios/dist/index.ts
@@ -5,3 +5,11 @@ export {
 } from "./../main/auth/login/index";
 export { EmailTransmissionApi } from "../main/auth/email";
 export { SignupApi, type SignupApiReqType } from "../main/auth/signup";
+export { PostAuthPostingApi } from "../main/post/auth/postPosting";
+export {
+  MainPageApi,
+  type MainPageApiResType,
+  type MainPageApiWatermelonLoadType,
+  type MainPageApiBannersType,
+  type MainPageApiVariableTravelRegionType
+} from "../main/main/mainPage";
