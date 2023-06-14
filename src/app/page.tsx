45096f58e2b4aa439ea063b4d525e9dd8ca11e81
ldh7228 <ldh7228@gmail.com>
2023-06-14 17:07:04 +0900

    Update Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,35 @@
 "use client";
 
+import { MainPageApi, MainPageApiResType } from "@/axios/main/main/mainPage";
 import HeaderDiv from "@/lib/components/Header";
 import MainPageBanner from "@/lib/components/MainPage/Banner";
 import LocationList from "@/lib/components/MainPage/LocationList";
 import WatermelonRoad from "@/lib/components/MainPage/WmRoad";
 import styled from "@emotion/styled";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState<MainPageApiResType>();
+
+  useEffect(() => {
+    MainPageApi().then((res) => {
+      setData(res);
+    });
+  }, []);
   return (
     <>
       <HeaderDiv />
-      <MainPageBanner />
-      <Container>
-        <WatermelonRoad />
-        <LocationList />
-      </Container>
+      {data ? (
+        <>
+          <MainPageBanner data={data.banners} />
+          <Container>
+            <WatermelonRoad data={data.watermelonLoad} />
+            <LocationList data={data.variableTravelRegion} />
+          </Container>
+        </>
+      ) : (
+        <></>
+      )}
     </>
   );
 }
